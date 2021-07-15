a32ff64382d033eacae1937cffc6ed2701572232
Greg Dodd <gregrdodd@gmail.com>
2021-07-15 13:46:52 -0400

    complete initial logstore

--- Sources/LogStore/LogStore.swift
+++ Sources/LogStore/LogStore.swift
@@ -1,3 +1,10 @@
+import Foundation
+
 struct LogStore {
-    var text = "Hello, World!"
+    static var log: [String] = []
+}
+
+public func printLog(_ string: String) {
+    print(string)
+    LogStore.log.append(string)
 }
